9e60a3458ecfb81e18fefd417f39986c707d8437
Vitaly Kondratiev <vitalkond@gmail.com>
2019-03-04 08:38:51 +0000

    adds css in js to button

--- packages/starter-app/src/components/App/App.tsx
+++ packages/starter-app/src/components/App/App.tsx
@@ -1,17 +1,23 @@
 import * as React from 'react';
 import { increment } from '@appfocused/common/lib/esm';
-import { Button } from '@appfocused/ui-components/dist/es';
+import { Button, ThemeProvider } from '@appfocused/ui-components/dist/es';
 import './App.css';
 import * as styles from './app.module.css';
 
+const theme = {
+  color: 'white'
+};
 export class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className={`${styles.blue} App-header`}>{increment(99)}</header>
-        <div>123</div>
-        <Button buttonText="hello" />
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <header className={`${styles.blue} App-header`}>
+            {increment(99)}
+            <Button buttonText="hello" borderRadius={20} />
+          </header>
+        </div>
+      </ThemeProvider>
     );
   }
 }
