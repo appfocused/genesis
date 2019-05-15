fe08b8b0b65835c41e29d4339e7e8de9589196d1
Vitaly Kondratiev <vitalkond@gmail.com>
2019-05-15 10:14:00 +0100

    [build-scripts] adds prod webpack

---
+++ packages/build-scripts/config/webpack.config.prod.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const fs = require('fs');
+const buildPaths = require('./build-paths');
+
+const indexPath = buildPaths.path.join(buildPaths.src, 'index.html');
+const hasTemplate = fs.existsSync(indexPath);
+
+module.exports = options => ({
+  entry: [require.resolve('react-dev-utils/webpackHotDevClient'), buildPaths.src],
+  mode: 'development',
+  bail: false,
+  output: {
+    filename: 'static/js/bundle.js',
+    path: buildPaths.dist
+  },
+
+  resolve: {
+    symlinks: true,
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+
+  devtool: 'cheap-module-source-map',
+
+  module: {
+    rules: [
+      require('./loaders/eslint')(),
+      require('./loaders/typescript')(),
+      require('./loaders/css')({ hasModules: false, isEnvProduction: true }),
+      require('./loaders/css')({ hasModules: true, isEnvProduction: true })
+    ]
+  },
+
+  plugins: [
+    hasTemplate &&
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: indexPath
+      }),
+    require('./plugins/typescript-checker')()
+  ].filter(Boolean)
+});
