79f4ac1e9cfb5d25cb14029daeec37400a5f6455
Vitaly Kondratiev <vitalkond@gmail.com>
2019-05-15 09:10:49 +0100

    fixes starter-app

--- packages/starter-app/src/components/App/App.tsx
+++ packages/starter-app/src/components/App/App.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { increment } from '@appfocused/common/lib/esm';
-import { ThemeProvider, Button, Intent, defaultTheme } from '@appfocused/ui-components/dist/es';
+import { ThemeProvider, defaultTheme, Intent } from '@appfocused/ui-components/dist/es';
+
 import './App.css';
 import * as styles from './app.module.css';
+import Button from '@appfocused/ui-components/dist/es/components/Button';
 
 export class App extends React.Component {
   render() {
@@ -11,7 +13,7 @@ export class App extends React.Component {
         <div className="App">
           <header className={`${styles.blue} App-header`}>
             {increment(99)}
-            <Button data-e2e="test" size="large" color={Intent.Primary}>
+            <Button data-e2e="test" size="large" intent={Intent.Primary}>
               <span>test</span>
             </Button>
           </header>
