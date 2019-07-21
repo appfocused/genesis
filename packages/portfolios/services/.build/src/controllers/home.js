c209b6909455c6c6849d688579501c014c4e1cc8
Vitaly Kondratiev <vitalkond@gmail.com>
2019-07-21 20:49:42 +0100

    remove serverless build files

--- packages/portfolios/services/.build/src/controllers/home.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.index = function (req, res) {
-    res.send('Portfolio Services v1');
-};
-//# sourceMappingURL=home.js.map
--- packages/portfolios/services/.build/src/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-var serverless = require('serverless-http');
-exports.handler = serverless(app_1.default);
-//# sourceMappingURL=server.js.map
