d194d55dbfcebf42fe3e130fd0852fdd31af220b
Vitaly Kondratiev <vitalkond@gmail.com>
2019-08-13 21:56:48 +0100

    [finance-app] global themed style

--- packages/finance-app-shell/src/components/Page/Page.style.ts
+++ packages/finance-app-shell/src/components/Page/Page.style.ts
@@ -3,9 +3,19 @@ import { makeStyles, createStyles } from '@appfocused/ui-components/dist/es';
 export const useStyles = makeStyles(({ typography, spacing, transitions, palette, shape }) => {
   return createStyles({
     root: {
-      backgroundColor: palette.background.default,
       position: 'relative',
       padding: spacing(2)
+    },
+
+    '@global': {
+      body: {
+        padding: 0,
+        margin: 0,
+        backgroundColor: palette.background.default,
+        fontFamily: typography.fontFamily,
+        fontSize: `${typography.fontSize}px`,
+        fontWeight: typography.fontWeightRegular
+      }
     }
   });
 });
